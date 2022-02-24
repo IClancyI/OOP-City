158ba582e1f9abeb5ad3510cdbc5570a9ee3ce6c
Jaric De Becker <jaric.debecker@gmail.com>
2022-02-24 14:50:40 +0100

    init oop1.6

---
+++ oop1.6/service/cityLoader.php
@@ -0,0 +1,51 @@
+<?php
+
+class cityLoader
+{
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    private function getPDO()
+    {
+        return CreateConnection();
+    }
+
+    /**
+     * @return City[]
+     */
+
+    public function getCitys()
+    {
+        $citys = array();
+
+        $citysData = $this->queryForCitys();
+
+        foreach ($citysData as $cityData) {
+            $citys[] = $this->createCityFromData($cityData);
+        }
+
+        return $citys;
+    }
+
+    private function createCityFromData(array $cityData)
+    {
+        $city = new City($cityData['img_title']);
+        $city->setImgId($cityData['img_id']);
+        $city->setImgFilename($cityData['img_filename']);
+        $city->setImgWidth($cityData['img_width']);
+        $city->setImgHeight($cityData['img_height']);
+
+        return $city;
+    }
+
+    private function queryForCitys()
+    {
+        $statement = $this->getPDO()->prepare('SELECT * FROM images');
+        $statement->execute();
+        $citysArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $citysArray;
+    }
+}
---
+++ oop1.6/service/messageService.php
@@ -0,0 +1,64 @@
+<?php
+class messageService
+{
+    private $errors;
+
+    private $input_errors;
+
+    private $infos;
+
+    public function __constructor()
+    {
+
+    }
+
+    public function CountErrors()
+    {
+
+    }
+
+    public function CountInputErrors()
+    {
+
+    }
+
+    public function CountInfos()
+    {
+
+    }
+
+    public function CountNewErrors()
+    {
+
+    }
+
+    public function CountNewInputErrors()
+    {
+
+    }
+
+    public function CountNewInfos()
+    {
+
+    }
+
+    public function GetInputErrors()
+    {
+
+    }
+
+    public function AddMessage($type, $msg, $key = null)
+    {
+
+    }
+
+    public function ShowErrors()
+    {
+
+    }
+
+    public function ShowInfos()
+    {
+
+    }
+}
