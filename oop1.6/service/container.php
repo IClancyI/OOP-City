c05312abb9c82353bde7aa3aa9698f6d040c6098
Jaric De Becker <jaric.debecker@gmail.com>
2022-02-24 15:02:51 +0100

    edited container

--- oop1.6/service/container.php
+++ oop1.6/service/container.php
@@ -2,56 +2,17 @@
 
 class Container
 {
-    private $configuration;
-
     private $pdo;
 
     private $cityLoader;
 
-    public function __construct(array $configuration)
-    {
-        $this->configuration = $configuration;
-    }
+    private $userLoader;
 
-    /**
-     * @return PDO
-     */
-    public function getPDO()
-    {
-        if ($this->pdo === null) {
-            $this->pdo = new PDO(
-                $this->configuration['db_dsn'],
-                $this->configuration['db_user'],
-                $this->configuration['db_pass']
-            );
+    private $DBManager;
 
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
+    private $logger;
 
-        return $this->pdo;
-    }
-
-    /**
-     * @return ShipLoader
-     */
-    public function getShipLoader()
-    {
-        if ($this->shipLoader === null) {
-            $this->shipLoader = new ShipLoader($this->getPDO());
-        }
-
-        return $this->shipLoader;
-    }
-
-    /**
-     * @return BattleManager
-     */
-    public function getBattleManager()
-    {
-        if ($this->battleManager === null) {
-            $this->battleManager = new BattleManager();
-        }
-
-        return $this->battleManager;
-    }
+    private $messageService;
 }
+
+?>
