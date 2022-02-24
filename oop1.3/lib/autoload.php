a6318c4d5583e1210a450743a1dcee42408935ef
Jaric De Becker <jaric.debecker@gmail.com>
2022-02-24 13:39:52 +0100

    small fix

--- oop1.3/lib/autoload.php
+++ oop1.3/lib/autoload.php
@@ -17,7 +17,7 @@ require_once __DIR__.'../service/cityLoader.php';
 require_once __DIR__.'../model/user.php';
 require_once __DIR__.'../service/userLoader.php';
 require_once __DIR__.'../service/DBManager.php';
-g
+
 session_start();
 
 require_once "access_control.php";
